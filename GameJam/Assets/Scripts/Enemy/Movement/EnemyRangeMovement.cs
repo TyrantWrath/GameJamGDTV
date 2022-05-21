1e5014c0e174afe79468a88a54306bcffc419188
MrEggroll69 <105930043+MrEggroll69@users.noreply.github.com>
2022-05-21 12:55:06 -0500

    EnemyMovement V1.2
    Added enemy range movement

---
+++ GameJam/Assets/Scripts/Enemy/Movement/EnemyRangeMovement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangeMovement : MonoBehaviour
+{
+
+    private Transform player = null;
+    private Vector3 playerDirection;
+
+    private bool runAway = false;
+    private bool isWithinRange = false;
+
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float rangeAttackDistance = 6f;
+    [SerializeField] private float runAwayDistance = 4f;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForWithinRange();
+        CheckForRunAway();
+        
+        if(!isWithinRange)
+        {
+            Movement();
+        }
+
+        if(runAway)
+        {
+            Movement();
+        }
+    }
+
+
+    private void CheckForWithinRange()
+    {
+        if(Vector3.Distance(player.position, transform.position) < rangeAttackDistance)
+        {
+            isWithinRange = true;
+        }
+        else
+        {
+            isWithinRange = false;
+        }
+    }
+
+    private void CheckForRunAway()
+    {
+        if(Vector3.Distance(player.position, transform.position) < runAwayDistance)
+        {
+            runAway = true;
+        }
+        else
+        {
+            runAway = false;
+        }
+    }
+
+    private void Movement()
+    {
+        playerDirection = (player.position - transform.position).normalized;
+        playerDirection.z = 0;
+
+        if(runAway)
+        {          
+            transform.Translate(-playerDirection * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(playerDirection * speed * Time.deltaTime);
+        }
+    }
+
+
+    public void SetTrueRunAway()
+    {
+        runAway = true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
