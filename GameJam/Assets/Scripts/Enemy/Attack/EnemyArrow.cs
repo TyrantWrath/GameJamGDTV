2560419b335e9fe364d4f54a989a9d28be50325c
SaiyamLohia <101941401+SaiyamLohia@users.noreply.github.com>
2022-05-24 17:26:59 +0530

    arrow destroy timer

--- GameJam/Assets/Scripts/Enemy/Attack/EnemyArrow.cs
+++ GameJam/Assets/Scripts/Enemy/Attack/EnemyArrow.cs
@@ -19,6 +19,8 @@ public class EnemyArrow : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         RotateSprite();
+
+        Destroy(gameObject, 5);
     }
     void FixedUpdate()
     {
