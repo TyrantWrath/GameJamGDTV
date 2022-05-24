bde8c9aec3adc4edc5363a10ec49118346c5ed14
SaiyamLohia <101941401+SaiyamLohia@users.noreply.github.com>
2022-05-24 22:31:57 +0530

    tiny changes

--- GameJam/Assets/Scripts/Enemy/Movement/EnemyMovement.cs
+++ GameJam/Assets/Scripts/Enemy/Movement/EnemyMovement.cs
@@ -65,6 +65,6 @@ public class EnemyMovement : MonoBehaviour
     public void SetTrueRunAway()
     {
         runAway = true;
-        //enemyAttack.Attack();
+        enemyAttack.Attack();
     }
 }
