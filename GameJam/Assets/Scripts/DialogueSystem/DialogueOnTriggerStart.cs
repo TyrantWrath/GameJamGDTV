ec2526546f81080fbf602a9e418b0fd09e3a2e86
MrEggroll69 <105930043+MrEggroll69@users.noreply.github.com>
2022-05-29 08:40:10 -0500

    Added Final Ghost Necromancer

---
+++ GameJam/Assets/Scripts/DialogueSystem/DialogueOnTriggerStart.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOnTriggerStart : MonoBehaviour
+{
+    public bool isGhost = false;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(isGhost)
+        {
+            if(other.tag == "Ghost Player")
+            {
+                GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+        }
+        else
+        {
+            if(other.tag == "Real Player")
+            {
+                GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+            
+        }
+    }
+}
